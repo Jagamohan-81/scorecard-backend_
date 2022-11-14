ce23fc047dbd1cfa2bdcbef9356bc55efa515b03
Jgamohan Panda <97526535+Jagamohan-81@users.noreply.github.com>
2022-11-14 14:55:53 +0530

    Last touch-up
    Last touch-up

---
+++ configuration/config.js
@@ -0,0 +1,21 @@
+const config ={
+    db:{
+        user: 'postgres',
+        host: 'localhost',
+        database: 'scorecard',
+        password: 'Lipun@12345',
+        port: 5432,
+    },
+    cryptR:{
+        secret:"my_secret_keyhjkiujlk"
+    },
+    jwt:{
+        secret:"my_secret_keyhjkiujlk"
+    }
+
+
+
+
+}
+
+module.exports =config;
